82c1cdc87763b2d95d335b1a8d7929d0716a0ba1
Herman Moreno <herman.moreno@crowdint.com>
2012-11-27 21:21:31 -0600

    Bare rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShowMeTheCommit::Application.config.secret_token = '8a69e17fff931335efbee09138c4102506f58918aad6350c6342df386cc8bda93a8249ecada86998226523c7c90f69a396f60ad8d4c9d86cc0336318d0f6347f'
